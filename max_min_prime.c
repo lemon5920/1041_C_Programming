bd6577c0dc6d26edf1d3d7fcc5a2e70b0a2c7abe
lemon5920 <lemon5920@gmail.com>
2016-11-04 16:40:48 +0800

    2016/11/04 河內塔

--- max_min_prime.c
+++ max_min_prime.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-#include <limits.h>
-#include <math.h>
+#include <stdio.h> 
+#include <limits.h> // 取得型態邊界值用 lib
+#include <math.h> // 數學運算用 lib
 int isPrime(int x) {
+    /* 
+     * 取得一個數值後，去算出是否為質數
+     * 是：return 1, 否：return 0.
+     */
     int i;
+    // 例外狀況預先判斷
     if (x == 1) {
         return 0;
     } else if (x == 2) {
         return 1;
     }
+    // 例外狀況外之數值判斷。演算法：判斷的數值不超過根號 `x`
     for (i = 2; i <= (int)sqrt(x); i++) {
         if(x % i == 0) {
             return 0;
