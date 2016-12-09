2bc85f0980c8fce1fbbb718f27edcff53857df7f
lemon5920 <lemon5920@gmail.com>
2016-12-10 02:50:32 +0800

    Fix while to for.

--- judge1051/judge2/judge2-1.c
+++ judge1051/judge2/judge2-1.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#include <math.h>
-int main()
-{
-    int n, sum = 0, i, num = 2; // n: 初始數, sum: n 次方總和, num: 除數
+int main() {
+    int n, sum = 0, i, num, power; // n: 初始數, sum: n 次方總和, num: 除數
     scanf("%d", &n);
     for (i = 1; i <= n; i++)
         sum += i * i;
     printf("Sum: %d\n", sum);
-    while (sum != 1) {
-        int power = 0;
+    for (num = 2; sum != 1; num++) {
+        power = 0;
         while (sum % num == 0) { // 將 num 除乾淨
             sum /= num;
             power++;
         }
         if (power != 0)
             printf("(%d ^ %d) ", num, power);
-        num++;
     }
     return 0;
 }
